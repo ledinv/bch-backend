86c920ec9c5b90d7ba212a0332bcbe32fb3fed28
ledinv <ledinvvv@gmail.com>
2025-04-12 18:58:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ API_KEY = os.environ.get("BCH_API_KEY")
 
 @app.route('/api/tipo-cambio-bch')
 def tipo_cambio_bch():
-    url = "https://bchapi-am.developer.azure-api.net/api/v1/indicadores/620/cifras?formato=json"
+    url = "url = "https://bchapi-am.developer.azure-api.net/api/v1/indicadores/cifras/620?formato=json"
     headers = {
         "clave": API_KEY
     }
